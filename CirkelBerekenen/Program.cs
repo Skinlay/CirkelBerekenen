8c6a97378b7c391fcaaf6f1dfa909c59f84b2cf9
Caitlin Bond <401307@vistacollege.nl>
2021-11-07 19:07:41 +0100

    kan de inhoud van een contaner uitrekenen kan de inhoud opervlachte van een cilinder uitrekenen

--- CirkelBerekenen/Program.cs
+++ CirkelBerekenen/Program.cs
@@ -21,6 +21,47 @@ namespace CirkelBerekenen
             Console.WriteLine(pi);
             */
 
+            /*
+            // lengte x breedten x hoogten in dm
+
+            Console.WriteLine("vul de lengte van de contaner in in dm");
+            double lengte = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("vul de breedten van de contaner in in dm");
+            double breedten = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("vul de hoogte van de contaner in in dm");
+            double hoogte = Convert.ToInt32(Console.ReadLine());
+
+            double inhoud = lengte * breedten * hoogte;
+
+            Console.WriteLine("de inhoud van de contaner in liters is: " + inhoud);
+
+
+            double seventig = (inhoud / 100) * 70;
+            Console.WriteLine("70% van de contaner in liters is: " + seventig + "L");
+
+            */
+
+            // inhoud cilinder = π * r2 * h 
+
+            Console.WriteLine("vul de radius van de cilinder in");
+            double radius = Convert.ToInt32(Console.ReadLine());
+            double Result = Math.Pow(radius, 2);
+
+
+            Console.WriteLine("vul de hoogte van de radius");
+            double hoogte = Convert.ToInt32(Console.ReadLine());
+
+            double inhoud = Math.PI * Result * hoogte;
+
+            Console.WriteLine("hoeveel prosent wil je de cilinder gevuld?");
+            double prosent = Convert.ToInt32(Console.ReadLine());
+
+            double seventig = (inhoud / 100) * prosent;
+            Console.WriteLine(prosent + "% van de cilinder is: " + seventig);
+
+
 
         }
     }
