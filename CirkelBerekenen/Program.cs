0d6446c7483f053a356b4f8ecc640bc05708801e
Caitlin Bond <401307@vistacollege.nl>
2021-11-07 17:59:25 +0100

    Add project files.

---
+++ CirkelBerekenen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CirkelBerekenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+            //double pi = Math.PI;
+            //Oppervlakte cirkel = π x straal^2
+
+            double straal = Convert.ToInt32(Console.ReadLine());
+            double ResultA = Math.Pow(straal, 2);
+
+
+            double pi = Math.PI * ResultA;
+
+            Console.WriteLine(pi);
+            */
+
+
+        }
+    }
+}
